43bdf71851356b851bff353109de744f40a887ea
Info-RocketryAtVirginiaTech <92947292+Info-RocketryAtVirginiaTech@users.noreply.github.com>
2021-12-27 17:44:47 -0500

    Trying Travis

--- pages/index.js
+++ pages/index.js
@@ -11,46 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <body>
-        <nav class="bg-white shadow-lg">
-          <div class="max-w-6xl mx-auto px-4">
-            <div class="flex justify-between">
-              <div class="flex space-x-7">
-                <div>
-                  <a href="#" class="flex items-center py-4 px-2">
-                    <img src="logo.png" alt="Logo" class="h-8 w-8 mr-2" />
-                    <span class="font-semibold text-gray-500 text-lg"
-                      >Navigation</span
-                    >
-                  </a>
-                </div>
-                <div class="hidden md:flex items-center space-x-1">
-                  <a
-                    href=""
-                    class="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold "
-                    >Home</a
-                  >
-                  <a
-                    href=""
-                    class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
-                    >Services</a
-                  >
-                  <a
-                    href=""
-                    class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
-                    >About</a
-                  >
-                  <a
-                    href=""
-                    class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
-                    >Contact Us</a
-                  >
-                </div>
-              </div>
-            </div>
-          </div>
-        </nav>
-      </body>
+      
 
       <main className={styles.main}>
         <h1 className={styles.title}>
